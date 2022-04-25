f39d4d2b04747d69188b7a2bd275e9882380ad7e
mi2703 <mohammedibrahim2703@gmail.com>
2022-04-25 23:34:18 +0530

    RandomCode

---
+++ com/phase1/pythonDS/randomstuff.py
@@ -0,0 +1,21 @@
+# Scope Of variables
+def spam():
+ eggs=30
+ print(eggs)
+
+
+if __name__ == '__main__':
+ #print('42' + 3)
+ print(2+2)
+# 5a= " hi "
+ #print(5a)
+ ##**** Variable Name cant start with Number
+ _spam = 5
+ print(_spam)
+ print( " space" , len(''))
+ print(str(3.14))
+ print(int('29'))
+ a=int('23')  + 23
+ print(a)
+ print('cats', 'dogs', 'mice', sep='|')
+ spam()
